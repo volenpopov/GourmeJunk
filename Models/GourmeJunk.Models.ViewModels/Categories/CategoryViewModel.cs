433b606b27196c144b3b6aa5a3ff805f7a5e40cc
volenpopov <volenpopov@abv.bg>
2019-07-14 11:54:18 +0300

    add CategoryViewModel

---
+++ Models/GourmeJunk.Models.ViewModels/Categories/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GourmeJunk.Data.Models;
+using GourmeJunk.Services.Mapping;
+
+namespace GourmeJunk.Models.ViewModels.Categories
+{
+    public class CategoryViewModel : IMapFrom<Category>
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
