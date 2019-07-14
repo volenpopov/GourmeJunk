7eec9f686a2e895b977b792039573909855a9665
volenpopov <volenpopov@abv.bg>
2019-07-14 10:58:21 +0300

    add Category Model

---
+++ Data/GourmeJunk.Data.Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using GourmeJunk.Data.Common.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace GourmeJunk.Data.Models
+{
+    public class Category : BaseDeletableModel<string>
+    {
+        [Required]
+        [Display(Name = "Category Name")]        
+        public string Name { get; set; }
+    }
+}
