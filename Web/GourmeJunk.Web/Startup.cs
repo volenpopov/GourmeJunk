2d30b73f2d145a8ff6abc3c705435aec2e05d5ae
volenpopov <volenpopov@abv.bg>
2019-07-14 10:22:30 +0300

    Add Admin Area

--- Web/GourmeJunk.Web/Startup.cs
+++ Web/GourmeJunk.Web/Startup.cs
@@ -25,7 +25,6 @@ namespace GourmeJunk.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<GourmeJunkDbContext>(options =>
@@ -56,7 +55,6 @@ namespace GourmeJunk.Web
             services.AddScoped<IDbQueryRunner, DbQueryRunner>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
