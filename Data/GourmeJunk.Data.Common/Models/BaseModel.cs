416f10296a2ef863daa173f4a80057c5ab627f57
volenpopov <volenpopov@abv.bg>
2020-03-08 21:45:12 +0200

    Revert "automatically generate primary keys"
    This reverts commit 009c36df2a566efe72adfdf2b62ab1430e3e07f2.

--- Data/GourmeJunk.Data.Common/Models/BaseModel.cs
+++ Data/GourmeJunk.Data.Common/Models/BaseModel.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GourmeJunk.Data.Common.Models
 {
     public class BaseModel<TKey> : IAuditableEntity
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public TKey Id { get; set; }
 
         public DateTime CreatedOn { get; set; }
