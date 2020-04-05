adde27162e172869e3034063ca2b1ba5be301ffa
volenpopov <volenpopov@abv.bg>
2020-04-05 21:08:23 +0300

    add attribute for db to automatically generating ids

--- Data/GourmeJunk.Data.Common/Models/BaseModel.cs
+++ Data/GourmeJunk.Data.Common/Models/BaseModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GourmeJunk.Data.Common.Models
 {
     public class BaseModel<TKey> : IAuditableEntity
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public TKey Id { get; set; }
 
         public DateTime CreatedOn { get; set; }
