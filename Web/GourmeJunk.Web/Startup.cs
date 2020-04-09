19451b256c96e1e05681941c1e1ef2f2e38f9348
volenpopov <volenpopov@abv.bg>
2020-04-09 22:32:03 +0300

    fix home redirect

--- Web/GourmeJunk.Web/Startup.cs
+++ Web/GourmeJunk.Web/Startup.cs
@@ -152,9 +152,9 @@ namespace GourmeJunk.Web
 
             app.UseEndpoints(
                endpoints =>
-               {
+               {                   
+                   endpoints.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-                   endpoints.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");               
                    endpoints.MapRazorPages();
                });
         }
